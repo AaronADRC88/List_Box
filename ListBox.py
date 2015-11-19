2881048e31fa78c292d86d49db2294c1558dc76c
AaronADRC88 <aferreiradominguez@danielcastelao.org>
2015-11-19 17:55:15 +0100

    funcionando

--- ListBox.py
+++ ListBox.py
@@ -23,13 +23,13 @@ class Lista_en_caja(Gtk.Window):
         caixaV_int = Gtk.Box(Gtk.Orientation.VERTICAL)
         caixaH.pack_start(caixaV_int, True, True, 0)
 
-        etiqueta = Gtk.Label("Hora e data automatics", xaling=0)
-        etiqueta2 = Gtk.Label("Require acceso a internet", xaling=0)
+        etiqueta = Gtk.Label("Hora e data automatics", xalign=0)
+        etiqueta2 = Gtk.Label("Require acceso a internet", xalign=0)
         caixaV_int.pack_start(etiqueta, True, True, 0)
         caixaV_int.pack_start(etiqueta2, True, True, 0)
 
         switch = Gtk.Switch()
-        switch.props.valing = Gtk.Align.CENTER
+        switch.props.valign = Gtk.Align.CENTER
         caixaH.pack_start(switch, False, True, 0)
 
         listBox.add(fila)
@@ -39,7 +39,7 @@ class Lista_en_caja(Gtk.Window):
         caixaH2 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
         fila2.add(caixaH2)
 
-        etiqueta3 = Gtk.Label("Permitir auto-actualizar", xaling=0)
+        etiqueta3 = Gtk.Label("Permitir auto-actualizar", xalign=0)
         check = Gtk.CheckButton()
         caixaH2.pack_start(etiqueta3, True, True, 0)
         caixaH2.pack_start(check, True, True, 0)
@@ -49,15 +49,19 @@ class Lista_en_caja(Gtk.Window):
         caixaH3 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
         fila3.add(caixaH3)
 
-        etiqueta4 = Gtk.Label("formato data", xaling=0)
+        etiqueta4 = Gtk.Label("formato data", xalign=0)
         combo = Gtk.ComboBoxText()
         combo.insert(0, "0", "24-h")
         combo.insert(1, "1", "AM/PM")
         caixaH3.pack_start(etiqueta4, True, True, 0)
         caixaH3.pack_start(combo, False, True, 0)
+        listBox.add(fila3)
 
 
 lista = Lista_en_caja()
+lista.set_position(Gtk.WindowPosition.CENTER_ALWAYS)
+lista.set_resizable(False)
 lista.connect("delete-event", Gtk.main_quit)
 lista.show_all()
+lista.set_decorated(True)
 Gtk.main()
